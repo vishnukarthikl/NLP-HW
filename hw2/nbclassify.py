921beb914a9c827c08468b7eee269dd099c934ae
Vishnu karthik <vishnukl@usc.edu>
2016-01-26 20:26:26 -0800

    classifying reviews

--- hw2/nbclassify.py
+++ hw2/nbclassify.py
@@ -1,3 +1,4 @@
+import operator
 import os
 import random
 import re
@@ -18,6 +19,8 @@ class LabelParameter:
         self.vocabulary_size += 1
 
     def probability(self, word):
+        if word not in self.word_count:
+            return 1.0
         return 1.0 * self.word_count[word] / self.vocabulary_size
 
     def all_probabilities(self):
@@ -26,9 +29,6 @@ class LabelParameter:
 
 class NaiveLearner:
     def __init__(self, data):
-        self.labelled_data = {}
-        self.labels = []
-        self.parameters = {}
         self.all_data = data
         self.populate_training_data(0.7)
         self.learn_parameters()
@@ -37,6 +37,9 @@ class NaiveLearner:
         return self.parameters[label]
 
     def populate_training_data(self, ratio):
+        self.labels = []
+        self.labelled_data = {}
+        self.test_data = {}
         for data in self.all_data:
             if data.label in self.labelled_data:
                 self.labelled_data[data.label].append(data)
@@ -45,10 +48,15 @@ class NaiveLearner:
                 self.labelled_data[data.label] = [data]
 
         for label, labeled_data in self.labelled_data.iteritems():
-            self.labelled_data[label] = [self.labelled_data[label][i] for i in
-                                         random.sample(range(len(labeled_data)), int(len(labeled_data) * ratio))]
+            indices_to_include = random.sample(range(len(labeled_data)), int(len(labeled_data) * ratio))
+            self.test_data[label] = []
+            for i in range(len(labeled_data)):
+                if i not in indices_to_include:
+                    self.test_data[label].append(self.labelled_data[label][i])
+            self.labelled_data[label] = [self.labelled_data[label][i] for i in indices_to_include]
 
     def learn_parameters(self):
+        self.parameters = {}
         for label in self.labels:
             self.parameters[label] = self.process(self.labelled_data[label], label)
 
@@ -65,11 +73,44 @@ class NaiveLearner:
     def probabilities(self, label):
         return self.parameters[label].all_probabilities()
 
+    def label_probability(self, label):
+        count = 0
+        for labelled_data in self.labelled_data.values():
+            count += len(labelled_data)
+
+        return 1.0 * len(self.labelled_data[label]) / count
+
+    def label_probabilities(self):
+        probabilities = {}
+        for label in self.labels:
+            probabilities[label] = self.label_probability(label)
+        return probabilities
+
+
+class NaiveClassifier:
+    def __init__(self, parameters, label_probabilities, to_classify):
+        self.parameters = parameters
+        self.label_probabilities = label_probabilities
+        self.to_classify = to_classify
+
+    def classify_all(self):
+        return map(self.classify, self.to_classify)
+
+    def classify(self, data):
+        post_probability = {}
+        for label, probability in self.label_probabilities.iteritems():
+            words = re.compile('\w+').findall(data.text)
+            post_probability[label] = reduce(lambda acc, word: self.parameters[label].probability(word), words,
+                                             probability)
+
+        return data, max(post_probability.iteritems(), key=operator.itemgetter(1))[0]
+
 
 class Review:
-    def __init__(self, text, label):
+    def __init__(self, text, label, path):
         self.label = label
         self.text = text
+        self.path = path
 
     def __str__(self):
         return self.label + ": " + self.text
@@ -82,8 +123,11 @@ class ReviewLoader:
             for file in files:
                 with open(os.path.join(root, file), 'r') as fin:
                     for lines in fin:
-                        reviews.append(Review(lines, label))
+                        reviews.append(Review(lines, label, os.path.join(root, file)))
         return reviews
+
+    def load_without_label(self, directory):
+        return self.load(directory, '')
 
 
 train_data_dir = sys.argv[1]
@@ -92,4 +136,8 @@ loader = ReviewLoader()
 negative_reviews = loader.load(train_data_dir + '/positive_polarity', '1')
 positive_reviews = loader.load(train_data_dir + '/negative_polarity', '0')
 learner = NaiveLearner(negative_reviews + positive_reviews)
-print learner.probabilities('0')
+test_data = reduce(lambda acc, x: acc + x, learner.test_data.values(), [])
+classifier = NaiveClassifier(learner.parameters, learner.label_probabilities(), test_data)
+result = classifier.classify_all()
+correct = reduce(lambda acc, x: acc + 1 if x[1] == x[0].label else acc, result, 0)
+print 1.0 * correct / len(test_data)
