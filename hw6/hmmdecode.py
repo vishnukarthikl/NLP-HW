c1392215411085321a923c93714934ef22acac0a
Vishnu karthik <vishnukl@usc.edu>
2016-04-08 12:53:34 -0700

    tail writing

--- hw6/hmmdecode.py
+++ hw6/hmmdecode.py
@@ -19,33 +19,39 @@ class HMMTagger:
 
     def process(self, line):
         words = line.split()
-        probability = {(state, 0): self.e(state, words[0]) for state in self.states}
-        backpointers = {(state, 0): "" for state in self.states}
+        probability = [{}] * len(words)
+        backpointers = [{}] * len(words)
+        probability[0] = {state: self.e(state, words[0]) for state in self.states}
+        backpointers[0] = {state: "" for state in self.states}
         for t in range(1, len(words)):
+            probability[t] = {}
+            backpointers[t] = {}
             for state in self.states:
                 (prob, prev) = self.find_max_prev_probability(probability, state, t, words)
-                probability[(state, t)] = prob
-                backpointers[(state, t)] = prev
+                probability[t][state] = prob
+                backpointers[t][state] = prev
         tags = [0] * len(words)
-        final_state_probabilities = {state: probability[(state, len(words) - 1)] for state in self.states}
-        tags[len(words) - 1] = max(final_state_probabilities, key=final_state_probabilities.get)
+        final_state_probabilities = max(
+                [(probability[len(words) - 1][final_state], final_state) for final_state in self.states])
+        tags[len(words) - 1] = final_state_probabilities[1]
         for i in reversed(range(1, len(words))):
-            tags[i - 1] = backpointers[(tags[i], i)]
+            tags[i - 1] = backpointers[i][tags[i]]
         return zip(words, tags)
 
     def find_max_prev_probability(self, probability, state, t, words):
-        # p = {}
-        # for prev in self.states:
-        #     prev_probability = probability[(prev, t - 1)]
-        #     transition = self.t(prev, state)
-        #     emission = self.e(state, words[t])
-        #     p[prev] = prev_probability + transition + emission
-        # return p
-        return max([(probability[(prev, t - 1)] * self.t(prev, state) * self.e(state, words[t]), prev) for prev in
-                self.states])
+        p = []
+        for prev in self.states:
+            prev_probability = probability[t - 1][prev]
+            transition = self.t(prev, state)
+            emission = self.e(state, words[t])
+            p.append((prev_probability + transition + emission, prev))
+        return max(p)
+        # return max([(probability[(prev, t - 1)] * self.t(prev, state) * self.e(state, words[t]), prev) for prev in
+        #         self.states])
 
     def e(self, current, next):
-        return mod_log(self.probability_for(self.emission, current, next))
+        p = self.probability_for(self.emission, current, next)
+        return mod_log(p)
 
     def t(self, current, next):
         return mod_log(self.probability_for(self.transition, current, next))
@@ -54,22 +60,20 @@ class HMMTagger:
         if current in matrix and next in matrix[current]:
             return matrix[current][next]
         else:
-            return 0
+            return 1e-20
 
+
+def make_string(result):
+    return " ".join(map(lambda r: r[0] + "/" + r[1], result))
 
 file_path = sys.argv[1]
 params = pickle.load(open("hmmmodel.txt", "rb"))
 hmmTagger = HMMTagger(params['transition'], params['emission'])
-output_lines = []
-with open(file_path, "r") as f:
-    for line in f:
-        result = hmmTagger.process(line)
-        join = " ".join(map(lambda r: r[0] + "/" + r[1], result))
-        print join
-        output_lines.append(join)
-print "done"
-with open("hmmoutput.txt", "w") as f:
-    f.truncate()
-    for output in output_lines:
-        f.write(output)
-        f.write("\n")
+
+
+with open("hmmoutput.txt", "w") as fout:
+    fout.truncate()
+    with open(file_path, "r") as fin:
+        for line in fin:
+            result = hmmTagger.process(line)
+            fout.write(make_string(result) + "\n")
